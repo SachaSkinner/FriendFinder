5fb10e49fa2f75d2215ace098335e1a7738bb227
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-12 15:02:23 -0500

    .gitignore

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,9 +3,9 @@
 // why in quotes""??
 var friendObject = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
             5,
             1,
             4,
@@ -16,11 +16,11 @@ var friendObject = [
             5,
             4,
             1
-          ]
-      }
-      
-  ];
-  
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendObject;
+        ]
+    }
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendObject;
---
+++ app/data/test.js
@@ -0,0 +1,4 @@
+var JSdata = require('./friends.js')
+var JSONdata = require('./friends.json')
+
+console.log(JSdata, JSONdata)
