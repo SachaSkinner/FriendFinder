b35fb56e9561160256f8dff1a49815f09ff2ce76
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-12 21:36:44 -0500

    Updated server

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -1,1 +1,15 @@
-console.log("Loaded")
+$(document).ready(function () {
+
+    $.ajax({
+        url: "/api/friends",
+        method: "POST",
+        data: {
+            name: "Ben",
+            photo: "img",
+            scores: [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        }
+    })
+        .then(function(result){
+            console.log(result)
+        })
+})
